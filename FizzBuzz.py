7cff369ae408991e80f28ed68a9d31950e9cc97a
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-10 21:45:01 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,34 @@
+"""
+FizzBuzz Problem
+
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of 3, print "Fizz" instead of the number, and for 
+the multiples of 5, print "Buzz". 
+For numbers which are multiples of both three and five, print "FizzBuzz".
+Each number should be printed on a new line.
+
+Example Output:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+...
+
+Solution Below:
+"""
+
 for i in range(1,101):
     if(i%3==0 and i%5==0):
         print("FizzBuzz")
