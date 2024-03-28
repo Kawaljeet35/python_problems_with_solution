34bd5dd39c3411e61ef44e29dc96c69340f9d90c
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 15:48:39 +0530

    Create mergeSort.py

---
+++ mergeSort.py
@@ -0,0 +1,40 @@
+"""
+Merge Sort
+
+Write a program in python to implement the Merge Sort algorithm and 
+sort a given list in ascending order. 
+"""
+
+#Solution:
+def mergeSort(list):
+    if len(list)>1:
+        mid = len(list) // 2
+        left_list = list[:mid]
+        right_list = list[mid:]
+        mergeSort(left_list)
+        mergeSort(right_list)
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] < right_list[j]:
+                list[k] = left_list[i]
+                i += 1
+                k += 1
+            elif left_list[i] > right_list[j]:
+                list[k] = right_list[j]
+                j += 1
+                k += 1
+        while i < len(left_list):
+            list[k] = left_list[i]
+            i += 1
+            k += 1
+        while j < len(right_list):
+            list[k] = right_list[j]
+            j += 1
+            k += 1
+    return list
+
+list = [8,3,7,4,1]
+print(mergeSort(list))
