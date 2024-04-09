6b04df268ec4c7e9dfa67e98779db709e37ce4eb
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-04-09 08:31:43 +0530

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,19 +6,20 @@ sort a given list in ascending order.
 """
 
 #Solution:
-def bubbleShort(list):
-    swapped = False
-    for i in range(len(list)-1):
-        for j in range(len(list)-1-i):
-            if list[j] > list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
+def bubbleSort(list):
+    for i in range(len(list) - 1):
+        swapped = False
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
                 swapped = True
         if not swapped:
             break
     return list
 
-list_a = [8,3,4,7,1,1]
-print(bubbleShort(list_a))
+list_a = [8, 3, 4, 7, 1, 1]
+print(bubbleSort(list_a))
+
 
 """
 Time Complexity:
